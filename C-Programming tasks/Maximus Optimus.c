8d5136197269de7b3c6133e89b3c7946b1de9d4a
sai harsha <94219897+Venkata-Sai-Harsha@users.noreply.github.com>
2022-06-09 17:42:28 +0530

    Update Maximus Optimus.c

--- C-Programming tasks/Maximus Optimus.c
+++ C-Programming tasks/Maximus Optimus.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+ int main() {
+     int a[3],n,p=0;
+     for(int i=0;i<3;i++)
+     scanf("%d",&a[i]);
+
+     scanf("%d",&n);
+
+     for(int j=0;j<n;j++){
+         int max = 0;
+         for(int i=1;i<3;i++){
+              if(a[max]<a[i])
+              max = i;
+         }
+         p=p+a[max];
+         a[max]--;
+     }
+     printf("%d",p);
+     return 0;
+
+ }
