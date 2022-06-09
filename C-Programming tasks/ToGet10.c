73f8917ca50d913ce06adaed0db684e7ba70d65e
sai harsha <94219897+Venkata-Sai-Harsha@users.noreply.github.com>
2022-06-09 17:44:15 +0530

    Update ToGet10.c

--- C-Programming tasks/ToGet10.c
+++ C-Programming tasks/ToGet10.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 int main(){
     int count=0,num,n=0,extra=0,sum=0;
     float avg;
     scanf("%d",&num);
+    
     int a[1000];
     do{
         scanf("%d",&a[count++]);
     }
+    
     while(getchar()!='\n' && count<num);
+    
     for(int i=0;i<count;i++){
         sum=sum+a[i];
     }
